3ee3c7b1b2dcb37717540fde499d8518e812315e
joshau kregg <kreggjoshua@gmail.com>
2023-03-21 14:55:33 -0400

    complete lab

--- index.js
+++ index.js
@@ -12,5 +12,15 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((titles) => {
+    const words = titles.split(" ");
+
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i][0].toUpperCase() + words[i].substring(1);
+    }
+
+    words.join(" ");
+    return words.toString().replace(/,/g, ' ');
+  })
 }
+
